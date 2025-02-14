bb5e071b3429eab2444ac4ee29ee09f263c8c855
Danilo de Santana Alcantara <danilo.alcantara.2003y@gmail.com>
2025-02-14 20:38:25 -0300

    adiciona navegação basica, muda cor dos botões

--- app/src/main/java/com/example/respondeai/ChoiceModeActivity.kt
+++ app/src/main/java/com/example/respondeai/ChoiceModeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.respondeai
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +23,18 @@ class ChoiceModeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
+
+    fun NavegarParaPlay(view:View){
+        startActivity(Intent(this, PlayQuestionsActivity::class.java))
+    }
+    fun NavegarParaAdicionarPergunta(view:View){
+        startActivity(Intent(this, AddQuestionActivity::class.java))
+
+    }
+    fun NavegarParaRank(view:View){
+        startActivity(Intent(this, RankActivity::class.java))
+    }
+
 }
